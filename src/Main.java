269f56e879697388437ffa0ba7c5d213377c8d2a
Roman Grigorev <gr78rus@gmail.com>
2022-10-14 17:37:17 +0300

    ver1.0

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+
+        do {
+            s = s.replace("()", "");
+        } while (s.contentEquals("()"));
+
+        if (s.isEmpty()) {
+            System.out.println("Верно");
+        } else {
+            System.out.println("Не верно");
+        }
+    }
+}
